316eb6c5b9cb15a33924589ce55b98ee488dfeba
Paulius Kurlavičius <paul.kurlavicius@gmail.com>
2023-05-08 14:00:46 +0300

    Fix pagination

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from requests.exceptions import RequestException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.getLogger('selenium').setLevel(logging.WARNING)
@@ -24,7 +26,7 @@ class Main:
     def get_data(self):
         logging.info("Starting the Selenium WebDriver and Fetching Data...")
         options = Options()
-        options.add_argument('--headless=new')
+        # options.add_argument('--headless=new')
         options.add_experimental_option('excludeSwitches', ['enable-logging']) # Disable log: "DevTools listening on..."
         driver = webdriver.Chrome(options=options)
         driver.get(self.url)
@@ -65,10 +67,14 @@ class Main:
                 results.append({'url': actual_url, 'symbol': symbol})
 
             try:
-                next_button = driver.find_element(By.XPATH, "//li[contains(@class, 'page-item') and contains(@class, 'page-link__next')]//span[contains(@class, 'page-link')]")
-                if "disabled" in next_button.find_element(By.XPATH, "./..").get_attribute("class"):
+                # Wait for the next button to be clickable, with a maximum wait time of 10 seconds
+                wait = WebDriverWait(driver, 10)
+                next_button = wait.until(EC.presence_of_element_located((By.XPATH, "//li[contains(@class, 'page-item') and contains(@class, 'page-link__next')]//*[self::button or self::span][contains(@class, 'page-link')]")))  # Modified XPath expression to look for either <button> or <span> element
+                if "disabled" in next_button.find_element(By.XPATH, "./..").get_attribute("class") or next_button.tag_name == 'span':
                     break
                 else:
+                    driver.execute_script("arguments[0].scrollIntoView();", next_button)
+                    time.sleep(1)
                     next_button.click()
                     time.sleep(2)  # Add a delay to allow the page to load the new data
             except NoSuchElementException:
