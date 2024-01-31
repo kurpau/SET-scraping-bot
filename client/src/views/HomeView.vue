aed8614e2804fd6c1f828924d784f31231706ca3
kurpau <paul.kurlavicius@gmail.com>
2024-01-31 14:53:59 +0200

    Add date picker

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,18 +5,34 @@
     <base-button>Last 5 days</base-button>
     <base-button @click="fetchStocks">Last month</base-button>
     <base-button>Last 3 months</base-button>
+    <base-button>Fetch Stocks</base-button>
+  </div>
+  <div class="date-container">
+    <label for="start">Start date:</label>
+    <input type="date" id="start" v-model="startDate" />
+
+    <label for="end">End date:</label>
+    <input type="date" id="end" v-model="endDate" />
   </div>
   <stock-list :stocks="stocks"></stock-list>
+  {{ stocks }}
 </template>
 
 <script setup>
 import { ref } from "vue";
 import StockList from "../components/StockList.vue";
 
+function formatTodayDate() {
+  const today = new Date();
+  return today.toISOString().substring(0, 10);
+}
+
 const stocks = ref([]);
+const startDate = ref(formatTodayDate());
+const endDate = ref(formatTodayDate());
 
 async function fetchStocks() {
-  const path = "http://localhost:5000/stocks?from=2023-12-27&to=2024-01-27";
+  const path = `http://localhost:5000/stocks?from=${startDate.value}&to=${endDate.value}`;
   const res = await fetch(path);
   const data = await res.json();
   stocks.value = [...data.stocks];
