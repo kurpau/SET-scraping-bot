0408e29a152dce9bfd0d960e415e6105213dc2ae
kurpau <paul.kurlavicius@gmail.com>
2024-03-08 16:36:46 +0200

    Simplify server logic

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,15 +1,6 @@
 from app import create_app
-from waitress import serve
-import webbrowser
-from threading import Timer
 
 app = create_app()
 
-
-def open_browser():
-    webbrowser.open_new("http://127.0.0.1:5000/")
-
-
 if __name__ == "__main__":
-    Timer(1, open_browser).start()  # Open a web browser window.
-    serve(app, host="0.0.0.0", port=5000)
+    app.run()
