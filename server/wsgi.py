77f837f35d1e9b83f361011fec903321bd6cdf71
paukur2 <paul.kurlavicius@gmail.com>
2025-01-13 22:52:15 +0200

    Open browser tab on server start

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,7 +1,13 @@
 from app import create_app
+import webbrowser
+import os
 
 app = create_app()
 
+def open_browser():
+    webbrowser.open('http://localhost:5000')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
+        open_browser()
+    app.run(host="0.0.0.0", debug=False, port=5000)
